740018fe36036adfdfdb0703a062cd52d411024a
Néstor Castanedo <117448485+Ncastanedo@users.noreply.github.com>
2024-08-01 13:51:16 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { DAppProvider, Mainnet } from '@usedapp/core';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Home from './pages/Home';
+import Create from './pages/Create';
+import Explore from './pages/Explore';
+import NFTDetail from './pages/NFTDetail';
+
+const config = {
+  readOnlyChainId: Mainnet.chainId,
+  readOnlyUrls: {
+    [Mainnet.chainId]: 'https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID'
+  },
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <DAppProvider config={config}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/detail" element={<NFTDetail />} />
+        </Routes>
+      </BrowserRouter>
+    </DAppProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
